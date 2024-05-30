0af88bb4984404cac2e6e74bd9192d32cf5aeb62
yantowsky <it.yantowsky@gmail.com>
2024-05-30 20:45:04 +0300

    Added commits

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let ladder = {
     step: 0,
-    up: function () {
+    up: function () { // підніматиме вас на одну сходинку
         this.step++;
         return this;
     },
-    down: function () {
+    down: function () { // опускатиме вас на одну сходинку
         this.step--;
         return this;
     },
-    showStep: function () { 
+    showStep: function () {  // показує поточну сходинку
         console.log(this.step);
         return this;
     }
 };
 
-ladder.up().up().down().showStep();
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+
+ladder.up().up().down().showStep(); // 1
